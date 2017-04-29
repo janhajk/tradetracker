c02e82adbfd8d0061bc5599f73827ece48313070
Jan <janhajk@gmail.com>
2017-04-29 19:59:15 +0000

    display data

--- public/app.js
+++ public/app.js
@@ -1,8 +1,5 @@
 (function(){
 
-   // Spalten ind er Reihenfolge der Darstellung
-   var columns = ['Name', 'Titel', 'Views'];
-
 
    document.addEventListener('DOMContentLoaded', function() {
       //Filter einblenden
@@ -16,9 +13,7 @@
       request.onload = function() {
          if(request.status >= 200 && request.status < 400) {
             var data = JSON.parse(request.responseText);
-            var positions = {};
-            for(let i in data) {
-            }
+            body.appendChild(btable(data));
             console.log(data);
          } else {
             // Error
@@ -30,4 +25,66 @@
       };
       request.send();
    });
+
+   // Spalten ind er Reihenfolge der Darstellung
+   var columns = ['Market', 'Asset', 'Pair', 'Amount', 'open', 'last', 'Tot BTC', 'Tot USD'];
+
+   var btable = function(positions) {
+      var t = document.createElement('table');
+      t.className = 'table-striped';
+      t.width = '100%';
+      t.maxWidth = '400px';
+      var thead = document.createElement('thead');
+      var tr = document.createElement('tr');
+      var th;
+      for (var c in columns) {
+         th = document.createElement('th');
+         th.innerHTML = columns[c];
+         tr.appendChild(th);
+      }
+      thead.appendChild(tr);
+      t.appendChild(thead);
+      var tbody = document.createElement('tbody');
+      for (let i in positions) {
+         tbody.appendChild(positionsRow(positions[i]));
+      }
+      t.appendChild(tbody);
+      return t;
+   };
+
+   var positionsRow = function(position) {
+      var cols = {};
+      for (var i in columns) {
+         cols[columns[i]] = {};
+      }
+      var tr = document.createElement('tr');
+      var ftd = function(html, align) {
+         if(typeof align==='undefined' ){
+            align = 'left';
+         }
+         var td = document.createElement('td');
+         if(typeof html === 'object') {
+            td.appendChild(html);
+         } else if(typeof html === 'undefined') {
+            td.innerHTML = '';
+         } else {
+            td.innerHTML = html;
+         }
+         td.style.textAlign = align;
+         return td;
+      };
+      cols.Market = ftd(position.name);
+      cols.Asset = ftd(position.counter);
+      cols.Pair = ftd(position.base + '_' + position.counter);
+      cols.Amount = ftd(position.amount, 'right');
+      cols.open = ftd(position.open, 'right');
+      cols.last = ftd(position.rates[0].last, 'right');
+      let btc = position.rates[0].last * position.amount;
+      cols['Tot BTC'] = ftd(btc, 'right');
+      cols['Tot USD'] = ftd(btc * 1330);
+      for (var i in cols) {
+         tr.appendChild(cols[i]);
+      }
+      return tr;
+   };
 })();
