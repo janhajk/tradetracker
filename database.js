24ca41b01a05185462aaa6545d20e4c6c74d527f
Jan <janhajk@gmail.com>
2017-04-29 18:47:24 +0000

    path fix

--- database.js
+++ database.js
@@ -25,41 +25,5 @@ var getPositions = function(pid, connection, callback) {
 };
 exports.getPositions = getPositions;
 
-var splitPair = function(pair) {
-   var p = pair.split('_');
-   return {
-      name: p[0].toUpperCase() + '_' + p[1].toUpperCase(),
-      base: p[0].toUpperCase(),
-      counter: p[1].toUpperCase()
-   };
-};
 
-var getAssetId = function(pair, mysqlconnection, callback){
-   var query = '';
-   query += 'SELECT * FROM assets WHERE ' +
-            'base LIKE \'' + pair.base + '\' AND ' +
-            'counter LIKE \'' + pair.counter + '\'' +
-            'LIMIT 0,1';
-   if (config.dev) utils.log(query);
-   mysqlconnection.query(query, function(err, rowsStats) {
-      if(err) {
-         utils.log(err)
-         callback(err);
-      }
-      else if (!rowsStats.length) {
-         // Create new asset if non existent
-         mysqlconnection.query('INSERT INTO assets SET ?', pair, function(e, results, fields){
-            if (e) callback(e);
-            if (config.dev) console.log(e);
-            if (config.dev) console.log(results);
-            callback(null, [{aid:results.insertId}]);
-         });
-      }
-      else {
-         utils.log(rowsStats);
-         callback(null, rowsStats);
-      }
-
-   });
-};
 
