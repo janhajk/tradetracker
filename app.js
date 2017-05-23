0c7b25e0b4e4b7fad53a5ee2d238a511b1d3cd66
Jan <janhajk@gmail.com>
2017-05-23 13:56:09 +0000

    small fix

--- app.js
+++ app.js
@@ -113,8 +113,8 @@ app.get('/position/:pid/edit', ensureAuthenticated, function(req, res) {
 
 
 app.get('/rates/all', ensureAuthenticated, function(req, res){
-   var rates = require(__dirname + '/rates.js');
-   rates.ratesPoloniexGet(function(error, rates){
+   var rates = require(__dirname + '/lib/rates.js');
+   rates.all(function(error, rates){
       if (error) res.send(error);
       else res.send(rates);
    });
