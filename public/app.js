e0a349433b27d04f7b120def17bac03b3492a56e
Jan <janhajk@gmail.com>
2017-06-14 10:28:51 +0000

    bug

--- public/app.js
+++ public/app.js
@@ -68,7 +68,7 @@
       this.last = position.last;
       this.update = setInterval(function(){
          for(let i in rates) {
-            if(rates[i].pair === position.pair && rates[i].mid === position.mid) {
+            if(rates[i].aid === position.aid && rates[i].cid === position.cid) {
                this.last = rates[i].last;
             }
          }
