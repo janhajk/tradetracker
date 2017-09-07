95eb7b2ddf05118ce6a9339e9c50ea772a78820d
Jan <janhajk@gmail.com>
2017-09-07 06:53:25 +0000

    bug

--- public/app.js
+++ public/app.js
@@ -884,7 +884,7 @@
                     var d = JSON.parse(request.responseText);
                     self.data = [];
                     for (let i=0;i<d.length;i++) {
-                        self.data.push([(new Date(d[i].timestamp*1000)), d[i].dollar]);
+                        self.data.push([d[i].timestamp, d[i].dollar]);
                     }
                     Highcharts.stockChart(this.chart, {
                         rangeSelector: {
