37459e7b2a91ec99197dad5d339832a346773276
Jan <janhajk@gmail.com>
2017-10-31 11:32:31 +0000

    routing and auth release

--- app.js
+++ app.js
@@ -8,15 +8,15 @@ if (dev !== undefined && dev) {
     utils.log('running in dev mode');
 }
 
+// Routing
+var routing = require(__dirname + '/routing.js');
 
+// Auth
+var auth = require(__dirname + '/auth.js');
 
 // System
 var path = require('path');
 var fs   = require('fs');
-
-// Auth
-var passport       = require('passport');
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 // Database
 var mysql = require('mysql');
@@ -49,95 +49,16 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(express.static((path.join(__dirname, 'public'))));
+
+
+// Auth-Routes
+auth.routing(app);
+
+// Routing
+routing.basic(app);
+
+
 app.listen(config.port, function () {
     utils.log('App runnung on port ' + config.port);
-});
-
-// Authentication
-passport.serializeUser(function(user, done) {
-    done(null, user);
-});
-passport.deserializeUser(function(obj, done) {
-    done(null, obj);
-});
-passport.use(new GoogleStrategy({
-    clientID: config.google.GOOGLE_CLIENT_ID,
-    clientSecret: config.google.GOOGLE_CLIENT_SECRET,
-    callbackURL: config.baseurl + '/auth/google/callback'
-}, function(accessToken, refreshToken, profile, done) {
-    utils.log(profile);
-    process.nextTick(function() {
-        if (profile.id === config.google.user) {
-            utils.log('Login in user "' + profile.displayName + '"');
-            return done(null, profile);
-        }
-        else {
-            utils.log('User not authorised!');
-            return done('user not authorised!');
-        }
-    });
-}));
-
-app.get('/auth/google', passport.authenticate('google', {scope: ['https://www.googleapis.com/auth/plus.login']}), function() {});
-app.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/'}), function(req, res) {
-    res.redirect('/');
-});
-app.get('/logout', function(req, res) {
-    req.logout();
-    res.redirect('/');
-});
-
-function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) { return next(); }
-    res.redirect('/');
-}
-
-// Router
-
-app.get('/', function(req, res){
-    fs.readFile(__dirname + '/public/index.html', 'utf-8', function (err, data){
-        res.send(data);
-    });
-});
-
-
-app.get('/position', ensureAuthenticated, function(req, res){
-    var positions = require(__dirname + '/lib/positions.js');
-    positions.get('all', connection, function(e, data){
-        res.send(e?e:data);
-    });
-});
-
-app.get('/history', ensureAuthenticated, function(req, res){
-    var history = require(__dirname + '/lib/history.js');
-    history.get(function(e, data){
-        res.send(e?e:data);
-    });
-});
-
-app.get('/rates', ensureAuthenticated, function(req, res){
-    // Get all rates live (mode=null); don't udpate db
-    rates.all(null, connection, function(e, data){
-        res.send(e?e:data);
-    });
-});
-
-app.get('/position/:pid/edit', ensureAuthenticated, function() {
-
-});
-
-
-app.get('/cron/:secret', function(req, res) {
-    if (req.params.secret===config.cronSecret) {
-        rates.all('write', connection, function(e){
-            if (e) res.send(e);
-            else res.send('All rates successfully updated!');
-        });
-    }
-    else {
-        res.send('not authorized!');
-    }
 });
---
+++ routing.js
@@ -0,0 +1,57 @@
+// User Config File
+var config = require(__dirname + '/config.js');
+
+var utils = require(__dirname + '/utils.js');
+var auth = require(__dirname + '/auth.js');
+var rates = require(__dirname + '/lib/rates.js');
+
+
+// System
+var path = require('path');
+var fs = require('fs');
+
+
+var basic = function(app) {
+    app.get('/', function(req, res) {
+        fs.readFile(__dirname + '/public/index.html', 'utf-8', function(err, data) {
+            res.send(data);
+        });
+    });
+
+    app.get('/position', auth.ensureAuthenticated, function(req, res) {
+        var positions = require(__dirname + '/lib/positions.js');
+        positions.get('all', connection, function(e, data) {
+            res.send(e ? e : data);
+        });
+    });
+
+    app.get('/history', auth.ensureAuthenticated, function(req, res) {
+        var history = require(__dirname + '/lib/history.js');
+        history.get(function(e, data) {
+            res.send(e ? e : data);
+        });
+    });
+
+    app.get('/rates', auth.ensureAuthenticated, function(req, res) {
+        // Get all rates live (mode=null); don't udpate db
+        rates.all(null, connection, function(e, data) {
+            res.send(e ? e : data);
+        });
+    });
+
+    app.get('/position/:pid/edit', auth.ensureAuthenticated, function() {
+
+    });
+
+    app.get('/cron/:secret', function(req, res) {
+        if(req.params.secret === config.cronSecret) {
+            rates.all('write', connection, function(e) {
+                if(e) res.send(e);
+                else res.send('All rates successfully updated!');
+            });
+        } else {
+            res.send('not authorized!');
+        }
+    });
+};
+exports.basic = basic;
